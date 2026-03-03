c9364843e3e3218801eadf003d261b27d56a62dc
somildholakia <somildholakia0852@gmail.com>
2026-03-03 17:04:37 +0530

    update

--- code.cpp
+++ code.cpp
@@ -2,13 +2,20 @@
 using namespace std;
 
 int main(){
-
-    int n;
-    cout<<"Enter the number";
+    int n,m;
+    string element = "";
+    cout<<"Enter the number of rows: ";
     cin>>n;
+    cout<<"Enter the number of elements per row: ";
+    cin>>m;
+    cout<<"Which element to be entered: ";
+    cin>>element;
 
-    for(int i = n; i<=n*10;i+=n){
-        cout<<i<<endl;
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=m;j++){
+            cout<<element<<" ";
+        }
+        cout<<endl;
     }
 
     return 0;
