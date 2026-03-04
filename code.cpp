6b39d68415a72e0853ec815578fc3a33476c109c
somildholakia <somildholakia0852@gmail.com>
2026-03-04 20:28:04 +0530

    update

--- code.cpp
+++ code.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main(){
 
-    int num = 1;
-    for(int row = 1;row <= 5;row++){
-        for(int col = 1;col <= 5; col++){
-            cout<<num<<" ";
-            num++;
+    for(int row = 4;row>=1;row--){
+        char name = 'A';
+        for(int col = 1;col<=row;col++){
+            cout<<name<<" ";
+            name++;
         }
         cout<<endl;
     }
