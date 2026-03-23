87bf962766c93bb24288165933406956a902286e
somildholakia <somildholakia0852@gmail.com>
2026-03-23 18:17:08 +0530

    update

--- code.cpp
+++ code.cpp
@@ -3,20 +3,34 @@ using namespace std;
 
 int main(){
 
-    int num;
-    cout<<"Enter the number to be Converted: ";
-    cin>>num;
-    int rem,mul = 1,ans = 0;
+    int n;
+    cout<<"Enter the number to be Checked: ";
+    cin>>n;
 
-    while(num){
-        rem = num % 2;
-        num /= 2;
-        ans = rem*mul + ans;
-        mul *= 10;
+    int original = n;
+    int count = 0;
+    while(n){
+        count++;
+        n /= 10;
     }
 
-    cout<<"The conversion is: "<<ans<<endl;
+    int temp = original;
+    int ans = 0,rem;
+    while(temp){
+        rem = temp % 10;
+        temp /= 10;
+        int power = 1;
+        for(int i = 0;i<count;i++){
+            power *= rem;
+        }
+        ans = power + ans;
+    }
 
+    if(ans = original){
+        cout<<"Its an armstrong number";
+    } else {
+        cout<<"Not an armstrong number";
+    }
 
     return 0;
 }
