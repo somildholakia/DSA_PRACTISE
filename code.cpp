d2803326468735dba4e7bd5ffca27bf7c9128044
somildholakia <somildholakia0852@gmail.com>
2026-03-29 19:42:07 +0530

    update

--- code.cpp
+++ code.cpp
@@ -3,60 +3,18 @@ using namespace std;
 
 int main(){
 
-    int arr[1000];
     int n;
-    cout<<"Enter how many numbers to be Inserted: ";
+    cout<<"Enter the terms till to find the sequence: ";
     cin>>n;
 
-    cout<<"Enter the numbers: "<<endl;
+    int fibo1 = 0,fibo2 = 1;
+    cout<<"The fibonacci sequence till "<<n<<"is: "<<endl;
     for(int i = 0;i<n;i++){
-        cin>>arr[i];
+        int ans = fibo1 + fibo2;
+        cout<<ans<<" ";
+        fibo1 = fibo2;
+        fibo2 = ans;
     }
 
-    cout<<"\nOriginal Array: "<<endl;
-    for(int i = 0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-
-    for(int i = 0;i<n-1;i++){
-        int minIndex = i;
-        for(int j = i + 1;j<n;j++){
-            if(arr[minIndex] > arr[j]){
-                minIndex = j;
-            }
-        }
-        swap(arr[minIndex],arr[i]);
-    }
-
-    cout<<"\nSorted Array: "<<endl;
-    for(int i = 0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-
-    int key;
-    cout<<"\nEnter the element to be found: ";
-    cin>>key;
-
-    int start = 0,index = -1;
-    int end = n -1;
-    while(start <= end){
-        int mid = start + (end - start)/2;
-
-        if(arr[mid] == key){
-            index = mid;
-            break;
-        } else if(arr[mid] < key){
-            start = mid + 1;
-        } else {
-            end = mid -1;
-        }
-    }
-
-    if(index >= 0){
-
-    cout<<"Element found at index: "<<index;
-    } else {
-        cout<<"Element not present";
-    }
     return 0;
 }
