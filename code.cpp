9b98e93ff1f4b65d4dde3d8fc923ce0034ce791c
somildholakia <somildholakia0852@gmail.com>
2026-04-14 13:24:32 +0530

    update

--- code.cpp
+++ code.cpp
@@ -1,30 +1,31 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
+
 
 int main(){
 
-    int arr[8] = {3,4,-2,5,8,20,-10,8};
-    int n = 8;
+    int arr[3][4] = {
+        2,3,4,5,
+        1,2,6,8,
+        4,9,3,2,
+    };
 
-    int isEqual = false;
-    int totalSum = 0;
+    int n = 3,m = 4;
     for(int i = 0;i<n;i++){
-        totalSum += arr[i];
-    }
-    int prefixSum = 0;
-    for(int i = 0;i<n-1;i++){
-        prefixSum += arr[i];
 
-       int suffix = totalSum - prefixSum;
-        if(prefixSum == suffix){
-            isEqual = true;
-            break;
-        }
+       int start = 0,end = m - 1;
+
+       while(start < end){
+        swap(arr[i][start],arr[i][end]);
+        start++,end--;
+       }
     }
-    if(!isEqual){
-        cout<<"Sum is not equal";
-    } else {
-        cout<<"Sum is equal";
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
     }
 
     return 0;
